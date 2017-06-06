5271e3c4be32f267ad9c71e57396083f71252118
youness <youness.abbassi@akka.eu>
2017-06-06 11:11:18 +0200

    add subscribe form and jquery validation

---
+++ TestFullStack.YA.Api/Scripts/actions/userScripts.js
@@ -0,0 +1,47 @@
+﻿ 
+$().ready(function () {
+
+    $("#formSubscribtion").validate({
+        rules: {
+            name: "required", 
+            login: {
+                required: true
+            },
+            password: {
+                required: true 
+            },
+            confirm_password: {
+                required: true,
+                equalTo: "#password"
+            },
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: "le nom est obligatoire !!",
+            login: {
+                required: "le login est obligatoire",
+                minlength: "Your username must consist of at least 2 characters"
+            },
+            password: {
+                required: "Obligatoire !!"
+            },
+            confirm_password: {
+                required: "Obligatoire !!",
+                equalTo: "les mots de passe ne sont pas identiques"
+            },
+            email: {
+                required: "l'email est obligatoire",
+                email:"Invalide email !!"
+            }
+                
+        }
+    });
+
+    $("#btnSubscribe").click(function () { 
+        $("#formSubscribtion").submit(); 
+    }); 
+    
+});
